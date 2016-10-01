e9bbd1e62e7a4faecbe4524f429e7fd9f29ab916
Hugo Rivera <hugo.rivera2319@gmail.com>
2016-10-01 11:32:09 -0600

    Remove QML context setting. Register NotificationClient and ClassReader as QML types.

--- src/main.cpp
+++ src/main.cpp
@@ -16,16 +16,9 @@
 
 int main(int argc, char *argv[])
 {
-    NotificationClient *notificationClient = new NotificationClient("", "Demo Notification");
-
-    QList<QObject*> listmodel;
-
-    ClassReader reader("/home/hroc/dev/qt/qtdemo/samples/classes.xml");
-    auto xmldoc = reader.loadXML();
-    // when loaded, convert the XML into a list of DataObjects
-    auto result = reader.parseClasses(xmldoc, listmodel);
-
-    listmodel.append(new DataObject("TEST", "location"));
+    // Register the ClassReader and NotificationClient types as QML types
+    qmlRegisterType<NotificationClient>("NMT.CREU2016.NotificationClient", 1,0, "NotificationClient");
+    qmlRegisterType<ClassReader>(       "NMT.CREU2016.ClassReaders", 1,0, "ClassReader");
 
     // start the QML app
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
@@ -34,18 +27,8 @@ int main(int argc, char *argv[])
     QQmlApplicationEngine engine;
     engine.load(QUrl(QLatin1String("qrc:/ui/main.qml")));
 
-    QQmlContext *ctxt = engine.rootContext();
-    ctxt->setContextProperty(QLatin1String("notificationClient"), notificationClient);
-    ctxt->setContextProperty(QLatin1String("listmodel"), QVariant::fromValue(listmodel));
-
-    if (result.failed) {
-        notificationClient->setNotification(result._failure);
-        ctxt->setContextProperty("error", result._failure);
-    } else {
-        notificationClient->setNotification(
-                QString("Parsed %1 classes!").arg(listmodel.length()));
-        ctxt->setContextProperty("error", "All is quiet on the western front.");
-    }
+    // QQmlContext *ctxt = engine.rootContext();
+    // ctxt->setContextProperty(QLatin1String("notificationClient"), &notificationClient);
 
     return app.exec();
 }
